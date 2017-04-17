2c0f3c7105c8b27e3c391cd38d1f60ab52599ea8
Pro100-Adm <kirow.alesha@yandex.ru>
2017-04-17 22:42:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from cgi import parse_qs
 html = '<form method="get">Ты знаешь что такое mmorpg?<input name="Answer1"></input><button>Next</button></form>'
 def wsgi_app(environ, start_response):
     status = '200 OK'
-    #d = parse_qs(environ['QUERY_STRING'])
-    #Answer1 = d.get('Answer1',[None])[0]
+    d = parse_qs(environ['QUERY_STRING'])
+    Answer1 = d.get('Answer1',[None])[0]
     x = []
-    #x.append(Answer1)
+    x.append(Answer1)
     response_headers = [('Content-type', 'text/html')]
     response_body = html
     #if Answer1:
