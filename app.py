969eba921aa97511770ed27940c68844ff882e03
Pro100-Adm <kirow.alesha@yandex.ru>
2017-04-18 18:15:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ def wsgi_app(environ, start_response):
     x.append(Answer8)
     if Answer1 and Answer2 and Answer3 and Answer4 and Answer5 and Answer6 and Answer7 and Answer8:
         for i in range(0,len(x)-1):
-            if x[i].isfloat:
-                x[i]=float(x[i])
+            x[i]=float(x[i])
         y=calc(x)  
         response_body="The Elder Scrolls Online: "+str(y[0])+"<br>"+"World of Warcraft: "+str(y[1])+"<br>"+"Revelations: "+str(y[2])+"<br>"+"Blade And Soul: "+str(y[3])+"<br>"+"EVE Online: "+str(y[4])+"<br>"+"Lineage 2: "+str(y[5])+"<br>"+"Skyforge: "+str(y[6])+"<br>"+"Аллоды Онлайн: "+str(y[7])+"<br>"+"'Star Wars: Knights of the Old Republic': "+str(y[8])+"<br>"+"Tera: "+str(y[9])+"<br>"
         start_response(status, response_headers)
