a90bfb6018a7b77e05ed402598e7c9e44840f250
Pro100-Adm <kirow.alesha@yandex.ru>
2017-04-18 09:13:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from cgi import parse_qs
-html = '<form method="get">Ты знаешь что такое mmorpg?<input name="Answer1"></input><button>Next</button></form>'
+html = """<form method="get">Ты знаешь что такое mmorpg?<input name="Answer1"></input>
+<form method="get">Ты играл в The Elder Scrolls 5: Skyrim?<input name="Answer2"></input>
+<form method="get">Ты играл в The Elder Scrolls 5 Skyrim?<input name="Answer3"></input>
+<form method="get">Ты играл в The Elder Scrolls 5 Skyrim?<input name="Answer4"></input>
+<form method="get">Ты играл в The Elder Scrolls 5 Skyrim?<input name="Answer5"></input>
+<form method="get">Ты играл в The Elder Scrolls 5 Skyrim?<input name="Answer6"></input>
+<form method="get">Ты играл в The Elder Scrolls 5 Skyrim?<input name="Answer7"></input>
+<form method="get">Ты играл в The Elder Scrolls 5 Skyrim?<input name="Answer8"></input><button>OK</button></form>"""
 def wsgi_app(environ, start_response):
+    x = []
     status = '200 OK'
     d = parse_qs(environ['QUERY_STRING'])
     Answer1 = d.get('Answer1',[None])[0]
-    x = []
-    x.append(Answer1)
+    Answer2 = d.get('Answer2',[None])[0]
+    Answer3 = d.get('Answer3',[None])[0]
+    Answer4 = d.get('Answer4',[None])[0]
+    Answer5 = d.get('Answer5',[None])[0]
+    Answer6 = d.get('Answer6',[None])[0]
+    Answer7 = d.get('Answer7',[None])[0]
+    Answer8 = d.get('Answer8',[None])[0]
+    x.append(Answer1, Answer2, Answer3, Answer4, Answer5, Answer6, Answer7, Answer8)
     response_headers = [('Content-type', 'text/html; charset=UTF-8')]
     response_body = html
-    if Answer1:
-        response_body = '<form method="get">Ты играл в The Elder Scrolls 5 Skyrim?<input name="Answer2"></input><button>Next</button></form>'+str(x)
-        d = parse_qs(environ['QUERY_STRING'])
-        Answer2 = d.get('Answer2',[None])[0]
-        x.append(Answer2)
     start_response(status, response_headers)
     yield response_body.encode()
     
