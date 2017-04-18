6e2a3373369ce604cf0eef35b4bf6c61fd9740fb
Pro100-Adm <kirow.alesha@yandex.ru>
2017-04-18 21:41:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from cgi import parse_qs
 from exp_syst_calc import *
-html = """<form method="get">Ты знаешь что такое mmorpg?<input name="Answer1"></input><br>
+html = """Введите значения в интервале от 0 до 1 в формате '0.1'.<br>
+<form method="get">Ты знаешь что такое mmorpg?<input name="Answer1"></input><br>
 <form method="get">Ты играл в The Elder Scrolls 5: Skyrim?<input name="Answer2"></input><br>
 <form method="get">Ты играл в Warcraft 3?<input name="Answer3"></input><br>
 <form method="get">Готов ли ты к ежемесячным платежам за игру?<input name="Answer4"></input><br>
 <form method="get">Готов ли ты к единовременному платежу за игру?<input name="Answer5"></input><br>
 <form method="get">Готов ли ты проводить большую часть времени в игре за скучными занятиями?<input name="Answer6"></input><br>
 <form method="get">Нравится ли тебе "корейская стилистика?<input name="Answer7"></input><br>
-<form method="get">Готов ли ты в любой момент потерять всё и начать сначала?<input name="Answer8"></input><br><button>OK</button></form>
-Введите значения в интервале от 0 до 1 в формате '0.1'.<br>"""
+<form method="get">Готов ли ты в любой момент потерять всё и начать сначала?<input name="Answer8"></input><br><button>OK</button></form>"""
 def wsgi_app(environ, start_response): 
     response_headers = [('Content-type', 'text/html; charset=UTF-8')]
     response_body = html
