5570f0ae8ddd571a221f2f7c8f3fe59bdb38dc1e
Pro100-Adm <kirow.alesha@yandex.ru>
2017-04-18 18:20:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ html = """<form method="get">Ты знаешь что такое mmorpg?<input n
 <form method="get">Готов ли ты проводить большую часть времени в игре за скучными занятиями?<input name="Answer6"></input><br>
 <form method="get">Нравится ли тебе "корейская стилистика?<input name="Answer7"></input><br>
 <form method="get">Готов ли ты в любой момент потерять всё и начать сначала?<input name="Answer8"></input><br><button>OK</button></form>
-Введите значения в интервале от 0 до 1 в формате '0.1'."""
+Введите значения в интервале от 0 до 1 в формате '0.1'.<br>"""
 def wsgi_app(environ, start_response): 
     response_headers = [('Content-type', 'text/html; charset=UTF-8')]
     response_body = html
