7464d519b843bda38aafae3f65f0848eccc8dbe5
Pro100-Adm <kirow.alesha@yandex.ru>
2017-04-18 10:06:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -70,8 +70,7 @@ def wsgi_app(environ, start_response):
                     Star = x[i]*list(ver_var_Star.values())[r]
             for s in range(0,len(ver_var_Tera)-1):
                 if i==list(ver_var_Tera.keys())[s]:
-                    Tera = x[i]*list(ver_var_Tera.values())[s]
-        response_body = html       
+                    Tera = x[i]*list(ver_var_Tera.values())[s]      
     start_response(status, response_headers)
     yield response_body.encode()
     
