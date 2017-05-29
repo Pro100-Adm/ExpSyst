999d3e05c8d00e2e9a3c866ba0b5dfafbf678c3e
Pro100-Adm <kirow.alesha@yandex.ru>
2017-05-29 18:28:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ html = """Введите значения в интервале от 0 до 1 в
 <form method="get">Готов ли ты проводить большую часть времени в игре за скучными занятиями?<input name="Answer6"></input><br>
 <form method="get">Нравится ли тебе "корейская стилистика?<input name="Answer7"></input><br>
 <form method="get">Готов ли ты в любой момент потерять всё и начать сначала?<input name="Answer8"></input><br>
+<form method="get">Введите термин, значение которого вы хотите узнать: <input name="word"></input><br>
 <button>OK</button></form>"""
 def wsgi_app(environ, start_response): 
     response_headers = [('Content-type', 'text/html; charset=UTF-8')]
@@ -28,6 +29,7 @@ def wsgi_app(environ, start_response):
     Answer6 = d.get('Answer6',[None])[0]
     Answer7 = d.get('Answer7',[None])[0]
     Answer8 = d.get('Answer8',[None])[0]
+    termin = d.get('word',[None])[0]
     x.append(Answer1)
     x.append(Answer2)
     x.append(Answer3)
@@ -40,10 +42,15 @@ def wsgi_app(environ, start_response):
         try:
             for i in range(0,len(x)-1):
                 x[i]=float(x[i])
-            y=calc(x)  
-            response_body="The Elder Scrolls Online: "+str(y[0])+"<br>"+"World of Warcraft: "+str(y[1])+"<br>"+"Revelations: "+str(y[2])+"<br>"+"Blade And Soul: "+str(y[3])+"<br>"+"EVE Online: "+str(y[4])+"<br>"+"Lineage 2: "+str(y[5])+"<br>"+"Skyforge: "+str(y[6])+"<br>"+"Аллоды Онлайн: "+str(y[7])+"<br>"+"'Star Wars: Knights of the Old Republic': "+str(y[8])+"<br>"+"Tera: "+str(y[9])+"<br>"
-            start_response(status, response_headers)
-            yield response_body.encode()
+            y=calc(x)
+            if termin:
+                response_body="The Elder Scrolls Online: "+str(y[0])+"<br>"+"World of Warcraft: "+str(y[1])+"<br>"+"Revelations: "+str(y[2])+"<br>"+"Blade And Soul: "+str(y[3])+"<br>"+"EVE Online: "+str(y[4])+"<br>"+"Lineage 2: "+str(y[5])+"<br>"+"Skyforge: "+str(y[6])+"<br>"+"Аллоды Онлайн: "+str(y[7])+"<br>"+"'Star Wars: Knights of the Old Republic': "+str(y[8])+"<br>"+"Tera: "+str(y[9])+"<br>"+"Termin: "+str(Dictionary.get(str(termin)))+"<br>"
+                start_response(status, response_headers)
+                yield response_body.encode()
+            else:
+                response_body="The Elder Scrolls Online: "+str(y[0])+"<br>"+"World of Warcraft: "+str(y[1])+"<br>"+"Revelations: "+str(y[2])+"<br>"+"Blade And Soul: "+str(y[3])+"<br>"+"EVE Online: "+str(y[4])+"<br>"+"Lineage 2: "+str(y[5])+"<br>"+"Skyforge: "+str(y[6])+"<br>"+"Аллоды Онлайн: "+str(y[7])+"<br>"+"'Star Wars: Knights of the Old Republic': "+str(y[8])+"<br>"+"Tera: "+str(y[9])+"<br>"
+                start_response(status, response_headers)
+                yield response_body.encode()
         except:
             response_body="Пожалуйста, введите корректные значения.<br>"
             start_response(status, response_headers)
